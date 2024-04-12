401c7acb614f5f559e327fdacc053aa0c0316e38
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2024-04-12 15:41:04 +0000

    Update C2/Server/install_dep.sh script

--- C2/Server/install_dep.sh
+++ C2/Server/install_dep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo apt-get install tor
-sudo service tor start
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+# apt update
+apt-get install tor -y
+service tor start
+apt-get install build-essential libssl-dev libffi-dev python-dev -y
+apt install inspircd -y
+nano /etc/inspircd/inspircd.conf
